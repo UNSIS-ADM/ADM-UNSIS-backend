310fd2daa67c0b225cc373041183ef0acf0d2d48
Sainos <qa.kadasoftware@gmail.com>
2025-05-14 00:56:06 -0600

    Initial commit

---
+++ src/main/java/com/unsis/ADM_UNSIS_backend/AdmUnsisBackendApplication.java
@@ -0,0 +1,13 @@
+package com.unsis.ADM_UNSIS_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdmUnsisBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdmUnsisBackendApplication.class, args);
+	}
+
+}

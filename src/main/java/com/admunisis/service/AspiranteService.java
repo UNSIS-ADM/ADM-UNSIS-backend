ec0ab3f2db5af3fe43c39e0cb7539f9cc187bd03
Sainos <qa.kadasoftware@gmail.com>
2025-05-20 23:31:45 -0600

    Implementación de la autenticación y autorización con JWT, incluyendo controladores y servicios para usuarios y aspirantes

---
+++ src/main/java/com/admunisis/service/AspiranteService.java
@@ -0,0 +1,18 @@
+﻿package com.admunisis.service;
+
+import com.admunisis.model.Aspirante;
+import com.admunisis.repository.AspiranteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AspiranteService {
+    @Autowired
+    private AspiranteRepository aspiranteRepository;
+    
+    public Aspirante guardarAspirante(Aspirante aspirante) {
+        return aspiranteRepository.save(aspirante);
+    }
+    
+    // Otros métodos de servicio
+}

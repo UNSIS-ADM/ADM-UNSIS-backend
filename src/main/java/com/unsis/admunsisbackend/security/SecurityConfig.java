80c7323d2627cc2c182bee7f2682d11f67a30e29
Sainos <qa.kadasoftware@gmail.com>
2025-07-28 11:04:16 -0600

    Modificar la implementación del PasswordEncoder para que no utilice encriptación, devolviendo el password sin cambios.

--- src/main/java/com/unsis/admunsisbackend/security/SecurityConfig.java
+++ src/main/java/com/unsis/admunsisbackend/security/SecurityConfig.java
@@ -78,9 +78,26 @@ public class SecurityConfig {
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
         return authConfig.getAuthenticationManager();
     }
-
+/*
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+}*/
+
+@Bean
+public PasswordEncoder passwordEncoder() {
+    // return new BCryptPasswordEncoder(); // Comentar esta línea
+    return new PasswordEncoder() {
+        @Override
+        public String encode(CharSequence rawPassword) {
+            return rawPassword.toString();
+        }
+
+        @Override
+        public boolean matches(CharSequence rawPassword, String encodedPassword) {
+            return rawPassword.toString().equals(encodedPassword);
+        }
+    };
+}
 }

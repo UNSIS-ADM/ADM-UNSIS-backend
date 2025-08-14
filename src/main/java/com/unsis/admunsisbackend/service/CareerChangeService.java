8d637cb98f138e4898004bd86032764f4b818bcd
Cantrax <carlos.cantrax303@gmail.com>
2025-08-14 09:54:45 -0600

    Merge branch 'main' of https://github.com/UNSIS-ADM/ADM-UNSIS-backend

--- src/main/java/com/unsis/admunsisbackend/service/CareerChangeService.java
+++ src/main/java/com/unsis/admunsisbackend/service/CareerChangeService.java
@@ -8,4 +8,7 @@ public interface CareerChangeService {
     CareerChangeRequestDTO submitChange(String username, CreateCareerChangeRequestDTO dto);
     List<CareerChangeRequestDTO> listPending();  // solo Admin/Secretaría
     CareerChangeRequestDTO processRequest(Long requestId, ProcessCareerChangeRequestDTO dto, String adminUsername);
+
+    List<CareerChangeRequestDTO> listByUsername(String username);
+
 }

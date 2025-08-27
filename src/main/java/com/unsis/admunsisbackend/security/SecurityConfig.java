e93dc9c6d626831f628ba12a68cb5e18803d603a
Cantrax <carlos.cantrax303@gmail.com>
2025-08-27 10:44:03 -0600

    Fix:docker inicial

--- src/main/java/com/unsis/admunsisbackend/security/SecurityConfig.java
+++ src/main/java/com/unsis/admunsisbackend/security/SecurityConfig.java
@@ -75,7 +75,8 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Collections.singletonList("http://localhost:4200"));
+        configuration.setAllowedOrigins(Collections.singletonList("http://localhost:4200")); 
+//        configuration.setAllowedOrigins(Collections.singletonList("*")); // Cambia esto por tu URL de frontend
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList(
                 "Authorization",
